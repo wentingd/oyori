64651692e0e9f55cf7a35127166d5be4c3091edf
duanwent <wenting.duan@axa.co.jp>
2018-12-05 16:16:46 +0900

    test

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const app = express();
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => res.status(200).json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();

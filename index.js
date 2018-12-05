3f8cad7b25d8c140799df82de4c2f26573b857a9
duanwent <wenting.duan@axa.co.jp>
2018-12-05 14:34:26 +0900

    update entry point & change server setup

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
 var async = require('async');
+var logger = require('morgan');
 
-app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(logger('dev'));
+
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
@@ -171,6 +173,6 @@ app.post('/callback',(req, res) => {
 
 // });
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running');
+app.listen(process.env.PORT || 8080, function() {
+    console.log('Node app is running on port :: ' + process.env.PORT || 8080);
 });

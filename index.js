f374757d5035a3ae599ffae5623f83be2f7a33ae
duanwent <wenting.duan@axa.co.jp>
2018-12-05 16:57:46 +0900

    try to identify event type

--- index.js
+++ index.js
@@ -1,46 +1,3 @@
-// const express = require('express');
-// const app = express();
-
-// const line = require("@line/bot-sdk");
-
-// if (process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config();
-// };
-
-// const lineConfig = {
-//     channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-//     channelSecret: process.env.LINE_CHANNEL_SECRET
-// };
-
-// const client = new line.Client(lineConfig);
-
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-// app.use(logger('dev'));
-
-// app.post('/webhook', line.middleware(lineConfig), (req, res, next) => {
-//     Promise
-//         .all(req.body.events.map(handleEvent))
-//         .then((result) => res.json(result))
-//         .catch((err) => {
-//             console.error(err);
-//             res.status(500).end();
-//         });
-// });
-
-// function handleEvent(event) {
-//     if (event.type !== 'message' || event.message.type !== 'text') {
-//         return Promise.resolve(null);
-//     }
-//     const echo = { type: 'text', text: event.message.text };
-//     return client.replyMessage(event.replyToken, echo);
-// }
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
-
 'use strict';
 
 const line = require('@line/bot-sdk');
@@ -51,7 +8,6 @@ if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 };
 
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
@@ -63,7 +19,6 @@ app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => {
-        console.log(result);
         res.status(200).send(result);
     })
     .catch((err) => {
@@ -72,16 +27,26 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     });
 });
 
-app.get('/ping', (req,res) => {
-    response.send('pong!');
+app.get('/', (req, res) => {
+    res.send('Hello oyori');
 });
 
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-  const echo = { type: 'text', text: event.message.text };
-  return client.replyMessage(event.replyToken, echo);
+    let { type } = event;
+    let reply = { type: 'text' };
+    switch (type) {
+        case 'text':
+            return client.replyMessage(event.replyToken, { ...reply, text: event.message.text });
+            break;
+        case 'image':
+            return client.replyMessage(event.replyToken, { ...reply, text: 'だれなんだろう?'});
+            break;
+        case 'video':
+            return client.replyMessage(event.replyToken, { ...reply, text: 'すみません、まだ動くものに慣れてなくて…'});
+            break;
+        default:
+            return Promise.resolve(null);
+      }    
 }
 
 const port = process.env.PORT || 8080;

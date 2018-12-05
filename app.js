20eecdf3b262fa78c33034b2f44e5690fbc532a9
duanwent <wenting.duan@axa.co.jp>
2018-12-05 14:15:46 +0900

    WIP

--- app.js
+++ app.js
@@ -1,41 +1,176 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var async = require('async');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.get('/', function(request, response) {
+    response.send('Hello World!');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/callback',(req, res) => {
+  console.log(req);
+  res.send('hello from call back');
 });
 
-module.exports = app;
+// app.post('/callback', function(req, res){
+
+//   async.waterfall([
+//       // ぐるなびAPI
+//       function(callback) {
+
+//           var json = req.body;
+
+//           // 受信テキスト
+//           var search_place = json['result'][0]['content']['text'];
+//           var search_place_array = search_place.split("\n");
+
+//           //検索キーワード
+//           var gnavi_keyword = "";
+//           if(search_place_array.length == 2){
+//               var keyword_array = search_place_array[1].split("、");
+//               gnavi_keyword = keyword_array.join();
+//           }
+
+//           // ぐるなびAPI レストラン検索API
+//           var gnavi_url = 'http://api.gnavi.co.jp/RestSearchAPI/20150630/';
+//           // ぐるなび リクエストパラメータの設定
+//           var gnavi_query = {
+//               "keyid":"<ぐるなびのアクセスキー>",
+//               "format": "json",
+//               "address": search_place_array[0],
+//               "hit_per_page": 1,
+//               "freeword": gnavi_keyword,
+//               "freeword_condition": 2
+//           };
+//           var gnavi_options = {
+//               url: gnavi_url,
+//               headers : {'Content-Type' : 'application/json; charset=UTF-8'},
+//               qs: gnavi_query,
+//               json: true
+//           };
+
+//           // 検索結果をオブジェクト化
+//           var search_result = {};
+
+//           request.get(gnavi_options, function (error, response, body) {
+//               if (!error && response.statusCode == 200) {
+//                   if('error' in body){
+//                       console.log("検索エラー" + JSON.stringify(body));
+//                       return;
+//                   }
+
+//                   // 店名
+//                   if('name' in body.rest){
+//                       search_result['name'] = body.rest.name;
+//                   }
+//                   // 画像
+//                   if('image_url' in body.rest){
+//                       search_result['shop_image1'] = body.rest.image_url.shop_image1;
+//                   }
+//                   // 住所
+//                   if('address' in body.rest){
+//                       search_result['address'] = body.rest.address;
+//                   }
+//                   // 緯度
+//                   if('latitude' in body.rest){
+//                       search_result['latitude'] = body.rest.latitude;
+//                   }
+//                   // 経度
+//                   if('longitude' in body.rest){
+//                       search_result['longitude'] = body.rest.longitude;
+//                   }
+//                   // 営業時間
+//                   if('opentime' in body.rest){
+//                       search_result['opentime'] = body.rest.opentime;
+//                   }
+
+//                   callback(null, json, search_result);
+
+//               } else {
+//                   console.log('error: '+ response.statusCode);
+//               }
+//           });
+
+//       },
+//   ],
+
+//   // LINE BOT
+//   function(err, json, search_result) {
+//       if(err){
+//           return;
+//       }
+
+//       //ヘッダーを定義
+//       var headers = {
+//           'Content-Type' : 'application/json; charset=UTF-8',
+//           'X-Line-ChannelID' : '<Your Channel ID>',
+//           'X-Line-ChannelSecret' : '<Your Channel Secret>',
+//           'X-Line-Trusted-User-With-ACL' : '<Your MID>'
+//       };
+
+//       // 送信相手の設定（配列）
+//       var to_array = [];
+//       to_array.push(json['result'][0]['content']['from']);
+
+
+//       // 送信データ作成
+//       var data = {
+//           'to': to_array,
+//           'toChannel': 1383378250, //固定
+//           'eventType':'140177271400161403', //固定
+//           "content": {
+//               "messageNotified": 0,
+//               "messages": [
+//                   // テキスト
+//                   {
+//                       "contentType": 1,
+//                       "text": 'こちらはいかがですか？\n【お店】' + search_result['name'] + '\n【営業時間】' + search_result['opentime'],
+//                   },
+//                   // 画像
+//                   {
+//                       "contentType": 2,
+//                       "originalContentUrl": search_result['shop_image1'],
+//                       "previewImageUrl": search_result['shop_image1']
+//                   },
+//                   // 位置情報
+//                   {
+//                       "contentType":7,
+//                       "text": search_result['name'],
+//                       "location":{
+//                           "title": search_result['address'],
+//                           "latitude": Number(search_result['latitude']),
+//                           "longitude": Number(search_result['longitude'])
+//                       }
+//                   }
+//               ]
+//           }
+//       };
+
+//       //オプションを定義
+//       var options = {
+//           url: 'https://trialbot-api.line.me/v1/events',
+//           proxy : process.env.FIXIE_URL,
+//           headers: headers,
+//           json: true,
+//           body: data
+//       };
+
+//       request.post(options, function (error, response, body) {
+//           if (!error && response.statusCode == 200) {
+//               console.log(body);
+//           } else {
+//               console.log('error: '+ JSON.stringify(response));
+//           }
+//       });
+
+//   });
+
+// });
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running');
+});

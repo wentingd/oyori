c1e37c8a4febf871a28a8da87644558a2f1810da
duanwent <wenting.duan@axa.co.jp>
2018-12-05 15:55:38 +0900

    echo config

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const request = require('request');
-const async = require('async');
 const logger = require('morgan');
 const line = require("@line/bot-sdk");
 
@@ -19,42 +17,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
+console.log(lineConfig.channelAccessToken);
+console.log(lineConfig.channelSecret);
+
 app.post('/webhook', line.middleware(lineConfig), (req, res, next) => {
     console.log(req.body);
-    res.status(200).json('success');
+    res.statusCode(200);
 });
-
-app.use((err, req, res, next) => {
-    if (err instanceof SignatureValidationFailed) {
-        res.status(401).send(err.signature)
-        return
-    } else if (err instanceof JSONParseError) {
-        res.status(400).send(err.raw)
-        return
-    }
-    next(err);
-});
-
-// app.post('/webhook', line.middleware(lineConfig), (req, res, next) => {
-//     res.sendStatus(200);
-//     let events_processed = [];
-
-//     req.body.events.forEach((event) => {
-//         if (event.type == "message" && event.message.type == "text"){
-//             if (event.message.text == "こんにちは"){
-//                 events_processed.push(bot.replyMessage(event.replyToken, {
-//                     type: "text",
-//                     text: "これはこれは"
-//                 }));
-//             }
-//         }
-//     });
-//     Promise.all(events_processed).then(
-//         (response) => {
-//             console.log(`${response.length} event(s) processed.`);
-//         }
-//     );
-// });
 
 app.get('/ping', function(request, response) {
     response.send('pong!');

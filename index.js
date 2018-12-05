786f3a139155ccd5ec5e65126c4f91c0fc8bd760
duanwent <wenting.duan@axa.co.jp>
2018-12-05 16:06:43 +0900

    update handler

--- index.js
+++ index.js
@@ -13,22 +13,35 @@ const lineConfig = {
     channelSecret: process.env.LINE_CHANNEL_SECRET
 };
 
+const client = new line.Client(lineConfig);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-console.log(lineConfig.channelAccessToken);
-console.log(lineConfig.channelSecret);
-
 app.post('/webhook', line.middleware(lineConfig), (req, res, next) => {
-    console.log(req.body);
-    res.statusCode(200);
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
 });
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+    const echo = { type: 'text', text: event.message.text };
+    return client.replyMessage(event.replyToken, echo);
+}
 
 app.get('/ping', function(request, response) {
     response.send('pong!');
 });
 
-app.listen(process.env.PORT || 8080, function() {
-    console.log('Node app is running on port :: ' + process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });

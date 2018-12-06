7af84919135af600dc9f2c7fd24d5298b6f8a022
duanwent <wenting.duan@axa.co.jp>
2018-12-06 16:13:25 +0900

    refactoring

--- index.js
+++ index.js
@@ -7,9 +7,6 @@
 // const morgan = require('morgan');
 // const app = express();
 
-// if (process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config();
-// };
 // const config = {
 //   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
 //   channelSecret: process.env.LINE_CHANNEL_SECRET,
@@ -23,16 +20,6 @@
 // }));
 // app.use(bodyParser.json());
 
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.status(200).json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
-
 // app.post('/mock/text', (req, res) => {
 //     const events = [{type: 'message', message: { type: 'text', text: req.body.message}}];
 //     Promise
@@ -43,61 +30,6 @@
 //         .catch(err => res.status(500).send('error'))
 // });
 
-// function handleEvent(event) {
-//   if (event.type !== 'message') {
-//     return Promise.resolve(null);
-//   }
-//   const reply = { type: 'text', text: event.message.text };
-//   return client.replyMessage(event.replyToken, reply);
-//   constructReplyMessage(event.message.type, event.message.text)
-//     .then(reply => {
-//         console.log(reply)
-//         client.replyMessage(event.replyToken, reply)
-//     })
-//     .catch(err => {console.log(err)})
-// }
-
-// const constructReplyMessage = async (msgType, msgText) => {
-//     console.log('constructreply ::' + msgText)
-//     switch (msgType) {
-//         case 'text':
-//             return { type: 'text', text: await giveRecommendation(msgText) };
-//             break;
-//         case 'image':
-//             return { type: 'text', text: 'これは何の写真なんだろう?' };
-//             break;
-//         case 'sticker':
-//             return { type: 'sticker', packageId: '11539', stickerId: '52114115' };
-//             break;
-//         case 'video':
-//             return { type: 'text', text: 'すみません、動くものはまだよくわからないのです...' };
-//             break;
-//         default:
-//             return { type: 'text', text: '私がまだ知らない何かですね。' };
-//     }
-// }
-
-// const giveRecommendation = async (msgText) => {
-//     console.log('constructreply ::' + msgText)
-//     const keywords = msgText.split(' ');
-//     let type = keywords[0];
-//     let text = keywords[1];
-//     if (text.indexOf('lord') > -1) {
-//         text = 'other,you,control,get,+1';
-//     }
-//     return await getFirstCardWithParam('cards?type=' + type + '&text=' + text);
-// }
-
-// const getFirstCardWithParam = async (param) => {
-//     console.log('constructreply ::' + param)
-//     const mtgApiUri = process.env.MTG_API_BASE_URI;
-//     return await request({ uri: mtgApiUri + param, json: true }).then(response => response.cards ? response.cards[0].name : '')
-// }
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`listening on ${port}`);
-// });
 
 'use strict';
 
@@ -107,6 +39,9 @@ const bodyParser = require('body-parser');
 const request = require('request-promise');
 const morgan = require('morgan');
 
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+};
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
@@ -119,7 +54,7 @@ const app = express();
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => res.status(200).json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
@@ -130,8 +65,6 @@ function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
-//   const echo = { type: 'text', text: event.message.text };
-//   return client.replyMessage(event.replyToken, echo);
   return constructReplyMessage(event.message.type, event.message.text)
     .then(reply => {
         console.log(reply)
@@ -141,7 +74,6 @@ function handleEvent(event) {
 }
 
 const constructReplyMessage = async (msgType, msgText) => {
-    console.log('constructreply ::' + msgText)
     switch (msgType) {
         case 'text':
             return { type: 'text', text: await giveRecommendation(msgText) };
@@ -161,7 +93,6 @@ const constructReplyMessage = async (msgType, msgText) => {
 }
 
 const giveRecommendation = async (msgText) => {
-    console.log('constructreply ::' + msgText)
     const keywords = msgText.split(' ');
     let type = keywords[0];
     let text = keywords[1];
@@ -172,9 +103,8 @@ const giveRecommendation = async (msgText) => {
 }
 
 const getFirstCardWithParam = async (param) => {
-    console.log('constructreply ::' + param)
     const mtgApiUri = process.env.MTG_API_BASE_URI;
-    return await request({ uri: mtgApiUri + param, json: true }).then(response => response.cards ? response.cards[0].name : '')
+    return await request({ uri: mtgApiUri + param, json: true }).then(response => response.cards[0] ? response.cards[0].name : '')
 }
 
 const port = process.env.PORT || 3000;

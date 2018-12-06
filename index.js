bf4700644adc23726bd081423339788b9df1570b
duanwent <wenting.duan@axa.co.jp>
2018-12-06 15:02:15 +0900

    debug

--- index.js
+++ index.js
@@ -39,15 +39,15 @@ app.get('/', (req, res) => {
     res.send('Hello oyori');
 });
 
-app.post('/mock/text', (req, res) => {
-    const events = [{type: 'message', message: { type: 'text', text: req.body.message}}];
-    Promise
-        .all(events.map(handleEvent))
-        .then(result => {
-            res.status(200).send(result.data.messages)
-        })
-        .catch(err => res.status(500).send('error'))
-});
+// app.post('/mock/text', (req, res) => {
+//     const events = [{type: 'message', message: { type: 'text', text: req.body.message}}];
+//     Promise
+//         .all(events.map(handleEvent))
+//         .then(result => {
+//             res.status(200).send(result.data.messages)
+//         })
+//         .catch(err => res.status(500).send('error'))
+// });
 
 const handleEvent = (event) => {
     if (!event.type || event.type !== 'message') {
@@ -60,41 +60,42 @@ const handleEvent = (event) => {
     }
 }
 
-const constructReplyMessage = async (msgType, msgText) => {
-    switch (msgType) {
-        case 'text':
-            return { type: 'text', text: await giveRecommendation(msgText) };
-            break;
-        case 'image':
-            return { type: 'text', text: 'これは何の写真なんだろう?' };
-            break;
-        case 'sticker':
-            return { type: 'sticker', packageId: '11539', stickerId: '52114115' };
-            break;
-        case 'video':
-            return { type: 'text', text: 'すみません、動くものはまだよくわからないのです...' };
-            break;
-        default:
-            return { type: 'text', text: '私がまだ知らない何かですね。' };
-    }
-}
+// const constructReplyMessage = async (msgType, msgText) => {
+//     switch (msgType) {
+//         case 'text':
+//             return { type: 'text', text: await giveRecommendation(msgText) };
+//             break;
+//         case 'image':
+//             return { type: 'text', text: 'これは何の写真なんだろう?' };
+//             break;
+//         case 'sticker':
+//             return { type: 'sticker', packageId: '11539', stickerId: '52114115' };
+//             break;
+//         case 'video':
+//             return { type: 'text', text: 'すみません、動くものはまだよくわからないのです...' };
+//             break;
+//         default:
+//             return { type: 'text', text: '私がまだ知らない何かですね。' };
+//     }
+// }
 
-const giveRecommendation = async (msgText) => {
-    const keywords = msgText.split(' ');
-    let type = keywords[0];
-    let text = keywords[1];
-    if (text.indexOf('lord') > -1) {
-        text = 'other,you,control,get,+1';
-    }
-    return await getFirstCardWithParam('cards?type=' + type + '&text=' + text);
-}
+// const giveRecommendation = async (msgText) => {
+//     const keywords = msgText.split(' ');
+//     let type = keywords[0];
+//     let text = keywords[1];
+//     if (text.indexOf('lord') > -1) {
+//         text = 'other,you,control,get,+1';
+//     }
+//     return await getFirstCardWithParam('cards?type=' + type + '&text=' + text);
+// }
 
-const getFirstCardWithParam = async (param) => {
-    const mtgApiUri = process.env.MTG_API_BASE_URI;
-    return await request({ uri: mtgApiUri + param, json: true }).then(response => response.cards ? response.cards[0].name : '')
-}
+// const getFirstCardWithParam = async (param) => {
+//     const mtgApiUri = process.env.MTG_API_BASE_URI;
+//     return await request({ uri: mtgApiUri + param, json: true }).then(response => response.cards ? response.cards[0].name : '')
+// }
 
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => {
   console.log(`listening on ${port}`);
 });

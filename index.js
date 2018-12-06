3dd363fa01532688faa1c6ecc04b181cb1e9ebf3
duanwent <wenting.duan@axa.co.jp>
2018-12-06 15:32:57 +0900

    debug

--- index.js
+++ index.js
@@ -120,47 +120,34 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
-// create LINE SDK client
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => res.status(200).json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
 });
 
-// event handler
 function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
+  if (event.type !== 'message') {
     return Promise.resolve(null);
   }
-
-  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
 
-// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+    console.log(`listening on ${port}`);
 });

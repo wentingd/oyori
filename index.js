878eaef3cc7dfb2f833b5cb6f262e9529f4e5578
duanwent <wenting.duan@axa.co.jp>
2018-12-06 17:34:54 +0900

    try again

--- index.js
+++ index.js
@@ -1,151 +1,101 @@
-// 'use strict';
-
-// const line = require('@line/bot-sdk');
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const request = require('request-promise');
-// const morgan = require('morgan');
-
-// if (process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config();
-// };
-// const config = {
-//   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.LINE_CHANNEL_SECRET,
-// };
-
-// const client = new line.Client(config);
-
-// const app = express();
-
-// app.use(morgan('tiny'));
-// app.use(bodyParser.urlencoded({
-//     extended: true
-// }));
-// app.use(bodyParser.json());
-
-// app.post('/mock/text', (req, res) => {
-//     const events = [{type: 'message', message: { type: 'text', text: req.body.message}}];
-//     Promise
-//         .all(events.map(handleEvent))
-//         .then(result => {
-//             res.status(200).send(result.data.messages)
-//         })
-//         .catch(err => res.status(500).send('error'))
-// });
-
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.status(200).json(result))
-//     .catch((err) => {
-//       console.error('Err in POST /callback :: ' + err);
-//       res.status(500).end();
-//     });
-// });
-
-// function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     return Promise.resolve(null);
-//   }
-//   return constructReplyMessage(event.message.type, event.message.text)
-//     .then(reply => {
-//         console.log(reply)
-//         client.replyMessage(event.replyToken, reply)
-//     })
-//     .catch(err => {console.error('Err in handleEvent :: ' + err);})
-// }
-
-// const constructReplyMessage = async (msgType, msgText) => {
-//     switch (msgType) {
-//         case 'text':
-//             return { type: 'text', text: await giveRecommendation(msgText) };
-//             break;
-//         case 'image':
-//             return { type: 'text', text: 'これは何の写真なんだろう?' };
-//             break;
-//         case 'sticker':
-//             return { type: 'sticker', packageId: '11539', stickerId: '52114115' };
-//             break;
-//         case 'video':
-//             return { type: 'text', text: 'すみません、動くものはまだよくわからないのです...' };
-//             break;
-//         default:
-//             return { type: 'text', text: '私がまだ知らない何かですね。' };
-//     }
-// }
-
-// const giveRecommendation = async (msgText) => {
-//     const keywords = msgText.split(' ');
-//     let type = keywords[0];
-//     let text = keywords[1];
-//     if (text.indexOf('lord') > -1) {
-//         text = 'other,you,control,get,+1';
-//     }
-//     let recommendation = await getFirstCardWithParam('cards?type=' + type + '&text=' + text);
-//     if (!recommendation) return 'sorry, no result found'
-//     return recommendation;
-// }
-
-// const getFirstCardWithParam = async (param) => {
-//     const mtgApiUri = process.env.MTG_API_BASE_URI;
-//     let result = await request({ uri: mtgApiUri + param, json: true })
-//         .then(response => response.cards[0] ? response.cards[0].name : '')
-//         .catch(err => console.log(err));
-//     if (!result) return 'sorry, no result found';
-//     return result;
-// }
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
-
 'use strict';
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request-promise');
+const morgan = require('morgan');
 
-// create LINE SDK config from env variables
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+};
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 
-// create LINE SDK client
 const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/mock/text', (req, res) => {
+    const events = [{type: 'message', message: { type: 'text', text: req.body.message}}];
+    Promise
+        .all(events.map(handleEvent))
+        .then(result => {
+            res.status(200).send(result.data.messages)
+        })
+        .catch(err => res.status(500).send('error'))
+});
+
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
+    .then((result) => res.status(200).json(result))
     .catch((err) => {
-      console.error(err);
+      console.error('Err in POST /callback :: ' + err);
       res.status(500).end();
     });
 });
 
-// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
     return Promise.resolve(null);
   }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return constructReplyMessage(event.message.type, event.message.text)
+    .then(reply => {
+        console.log(reply)
+        client.replyMessage(event.replyToken, reply)
+    })
+    .catch(err => {console.error('Err in handleEvent :: ' + err);})
 }
 
-// listen on port
+const constructReplyMessage = async (msgType, msgText) => {
+    switch (msgType) {
+        case 'text':
+            return { type: 'text', text: await giveRecommendation(msgText) };
+            break;
+        case 'image':
+            return { type: 'text', text: 'これは何の写真なんだろう?' };
+            break;
+        case 'sticker':
+            return { type: 'sticker', packageId: '11539', stickerId: '52114115' };
+            break;
+        case 'video':
+            return { type: 'text', text: 'すみません、動くものはまだよくわからないのです...' };
+            break;
+        default:
+            return { type: 'text', text: '私がまだ知らない何かですね。' };
+    }
+}
+
+const giveRecommendation = async (msgText) => {
+    const keywords = msgText.split(' ');
+    let type = keywords[0];
+    let text = keywords[1];
+    if (text.indexOf('lord') > -1) {
+        text = 'other,you,control,get,+1';
+    }
+    let recommendation = await getFirstCardWithParam('cards?type=' + type + '&text=' + text);
+    if (!recommendation) return 'sorry, no result found'
+    return recommendation;
+}
+
+const getFirstCardWithParam = async (param) => {
+    const mtgApiUri = process.env.MTG_API_BASE_URI;
+    let result = await request({ uri: mtgApiUri + param, json: true })
+        .then(response => response.cards[0] ? response.cards[0].name : '')
+        .catch(err => console.log(err));
+    if (!result) return 'sorry, no result found';
+    return result;
+}
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);

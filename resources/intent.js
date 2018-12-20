060c1462498e170901e440fc164cd67d24309db7
duanwent <wenting.duan@axa.co.jp>
2018-12-20 18:19:10 +0900

    add intent for greeting

---
+++ resources/intent.js
@@ -0,0 +1,16 @@
+const greeting = [
+    "wow",
+    "bored",
+    "none",
+    "hello",
+    "test",
+    "hi",
+];
+
+const mtg = [
+    "play",
+    "card",
+    "mtg"
+];
+
+module.exports = { greeting, mtg };

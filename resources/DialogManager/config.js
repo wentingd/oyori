ecaf065710401b9740bf74df6d54bf00864bcf8a
duanwent <wenting.duan@axa.co.jp>
2019-01-18 18:26:11 +0900

    update end of dialog msg

--- resources/DialogManager/config.js
+++ resources/DialogManager/config.js
@@ -1,7 +1,7 @@
 const dialogConfig = {
     noStepPromptMsg: 'ステップが設定されません',
     cancelDialogMsg: 'わかりました、この対話をストップしますね',
-    endDialogMsg: '以上です:)　\n"menu"とタイプすると、改めて私が出来ることをおさらいできますよー'
+    endDialogMsg: 'メニューって打つと、私が出来ることをおさらいできますよー'
 }
 
 module.exports = {
